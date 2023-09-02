0e13aa53bc5005b117b699c164c9a582c3bae724
Colby <60820431+ComputerTech312@users.noreply.github.com>
2023-09-02 20:04:05 +0100

    Update stuff.py

--- plugins/stuff.py
+++ plugins/stuff.py
@@ -1,5 +1,6 @@
 from src.plugin_base import PluginBase
 from src.channel_manager import ChannelManager
+import sys
 
 class Plugin(PluginBase):
     def __init__(self, bot):
@@ -10,9 +11,6 @@ class Plugin(PluginBase):
         message_parts = message.split()
         
         if message_parts[0] == '&join':
-            if len(message_parts) == 1:
-                self.bot._ircsend(f'JOIN {channel}')
-            else:
                 self.channel_manager.save_channel(message_parts[1])
                 self.bot._ircsend(f'JOIN {message_parts[1]}')
                 
@@ -22,3 +20,13 @@ class Plugin(PluginBase):
             else:
                 self.channel_manager.remove_channel(message_parts[1])
                 self.bot._ircsend(f'PART {message_parts[1]}')
+
+        elif message_parts[0] == "&quit":
+            if len(message_parts) == '0':
+                quit_message = 'EliteBot!'
+            else:
+                quit_message = message[len(message_parts[0])+1:]
+            self.bot._ircsend(f'QUIT {quit_message}')
+            self.bot.ircsock.close()
+            self.bot.connected = False
+            sys.exit()
