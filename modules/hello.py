261e28ef074357f73865d3c5762e68cb8671e811
Colby <60820431+ComputerTech312@users.noreply.github.com>
2023-09-02 20:02:24 +0100

    Delete modules directory

--- modules/hello.py
+++
@@ -1,9 +0,0 @@
-from src.plugin_base import PluginBase
-
-class Plugin(PluginBase):
-    def __init__(self, bot_instance):
-        super().__init__(bot_instance)
-        self.register_command("!hello", self.say_hello)
-        
-    def say_hello(self, source_nick, channel, message):
-        self.bot._ircsend(f'PRIVMSG {channel or source_nick} :Hello!')
