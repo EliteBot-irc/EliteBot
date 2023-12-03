049f3b76ec0462c5cbdeba8d639a123c1becda91
Colby <computertech312@gmail.com>
2023-12-03 06:51:11 +0000

    Update and rename hello.py to commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,29 @@
+from src.plugin_base import PluginBase
+from src.channel_manager import ChannelManager
+import sys
+
+class Plugin(PluginBase):
+        
+    def handle_message(self, source_nick, channel, message):
+        message_parts = message.split()
+        self.channel_manager = ChannelManager()  
+        if message_parts[0] == '!hello':
+            self.bot._ircsend(f'PRIVMSG {channel} :Hello, {source_nick}!')
+   
+        elif message_parts[0] == '!join':
+            self.channel_manager.save_channel(message_parts[1])
+            self.bot._ircsend(f'JOIN {message_parts[1]}')
+
+        elif message_parts[0] == '!part':
+            self.channel_manager.remove_channel(message_parts[1])
+            self.bot._ircsend(f'PART {message_parts[1]}')
+
+        elif message_parts[0] == '!quit':
+            if len(message_parts) == 1:
+                quit_message = 'EliteBot!'
+            else:
+                quit_message = message[len(message_parts[0])+1:]
+            self.bot._ircsend(f'QUIT :{quit_message}')
+            self.bot.ircsock.close()
+            self.bot.connected = False
+            sys.exit()
--- plugins/hello.py
+++
@@ -1,7 +0,0 @@
-from src.plugin_base import PluginBase
-
-class Plugin(PluginBase):
-        
-    def handle_message(self, source_nick, channel, message):
-        if message == '!hello':
-            self.bot._ircsend(f'PRIVMSG {channel} :Hello, {source_nick}!')
